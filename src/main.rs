f3e355235894e4c1c35a323a91301f986cd00ab1
Awildo <agutierr@hamilton.edu>
2020-05-25 13:54:59 -0400

    Day 1.2 Finished We accounted for the fuel required to carry the fuel required for each module.

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,32 @@ use std::io; // to read file name from input
 
 // Helper function that returns the amount of fuel required for a module of mass mod_mass.
 // Algorithm: Take its mass (a u32), divide by three, round down, and subtract 2
-fn fuel_required(mod_mass: u32) -> u32 {
+fn fuel_alg(mod_mass: i32) -> i32 {
     // Turns the parameter to f64, divides by 3 and rounds down.
     // Then, we subtract 2 and return that value
+
+    // Don't take negative inputs
+    if mod_mass < 0 {
+        return 0;
+    }
 
     let first_step = (mod_mass / 3) as f64;
     let second_step = first_step.floor();
 
-    (second_step as u32) - 2
+    (second_step as i32) - 2
+}
+
+// Calculates the fuel required 
+fn fuel_required(mod_mass: i32) -> i32 {
+    let fuel_req: i32 = fuel_alg(mod_mass);
+    // negative fuel
+    if fuel_req < 0 {
+        return 0;
+    }
+    // positive fuel
+    else {
+        return fuel_req + fuel_required(fuel_req);
+    }
 }
 
 // The Main method
@@ -53,8 +71,10 @@ fn main() {
     // for each line, calculate the fuel required for each module and add it to the total
     for line in contents.iter() {
         // parse() returns a Result type, expect will output an error message if it evaluates to err
-        let module_mass: u32 = line.trim().parse().expect("Problems!");
+        let module_mass: i32 = line.trim().parse().expect("Problems!");
+
         total_fuel += fuel_required(module_mass);
     }
+
     println!("Total Fuel Required: {}", total_fuel)
 }
